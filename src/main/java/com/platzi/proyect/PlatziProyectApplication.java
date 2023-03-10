76194777cf2807ebd446966af50d26345756172c
benjamin <monterobenja.2018@gmail.com>
2023-03-10 14:43:30 -0300

    subo todo lo principal

---
+++ src/main/java/com/platzi/proyect/PlatziProyectApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/proyect/PlatziProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
